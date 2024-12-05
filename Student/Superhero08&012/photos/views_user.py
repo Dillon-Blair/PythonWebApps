651604c73bef0a7ba22ad880277ecbee373a3649
Dillon-Blair <dblair88955@gmail.com>
2024-12-04 18:06:40 -0700

    General Changes for Project 8 & 12

---
+++ Student/Superhero08&012/photos/views_user.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, RedirectView, UpdateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm #Default stuff
+
+from .models import Author
+
+
+class UserHomeView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        if self.request.user.is_anonymous:
+            return '/author/'
+        author = Author.get_me(self.request.user)
+        if author.name == ' ':
+            author.user.first_name = 'Unknown Author Name'
+            author.user.last_name = 'Defualt UserName'
+            author.user.email = 'me@unco.us.com'
+            author.user.save()
+        return f'/author/{author.pk}'
+
+
+class UserAddView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/add.html'
+
+
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = "registration/edit.html"
+    model = User
+    fields = ['first_name', 'last_name', 'username', 'email'] #This is included in the editing user settings page
+    success_url = reverse_lazy('author_home')
--- Student/Superhero08/photos/tests_author.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
-from django.test import TestCase
-from django.urls import reverse
-
